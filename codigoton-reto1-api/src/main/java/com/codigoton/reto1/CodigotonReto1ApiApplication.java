d08ee43b6954d6e3c48fb38c74c75922e1328c66
ccastano <ccastano@192.168.1.5>
2021-10-22 19:09:20 -0500

    mi primer commit.

---
+++ codigoton-reto1-api/src/main/java/com/codigoton/reto1/CodigotonReto1ApiApplication.java
@@ -0,0 +1,13 @@
+package com.codigoton.reto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodigotonReto1ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodigotonReto1ApiApplication.class, args);
+	}
+
+}
---
+++ codigoton-reto1-api/src/test/java/com/codigoton/reto1/CodigotonReto1ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigoton.reto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodigotonReto1ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
